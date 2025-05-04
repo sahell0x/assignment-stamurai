0cf22767c85debfc4afcc009e2cf848674d7fa1b
Nayan Lakhwani <sahil.sk.khan6872@gmail.com>
2025-05-04 14:29:10 +0530

    backend init

---
+++ backend/src/index.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
+import { Request, Response, NextFunction } from "express";
+
+const app = express();
+const port: number = Number(process.env.PORT) || 3000;
+const dbUrl: string = process.env.DB_URL as string;
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+// app.use(cors({ origin: '*' }));
+
+app.use(express.json({ limit: "5mb" }));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/helloworld", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+const server = app.listen(port, () => {
+  console.log(`server is runnig at port:${port}`);
+});
+
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("connected to DB successfully");
+  })
+  .catch((err) => {
+    console.log("error while connecting");
+  });
