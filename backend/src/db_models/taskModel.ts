95d47debf4c81f23e67ccff65819a85325a85039
Nayan Lakhwani <sahil.sk.khan6872@gmail.com>
2025-05-05 22:52:47 +0530

    created task model

---
+++ backend/src/db_models/taskModel.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  dueDate: { type: Date, required: true },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "medium",
+  },
+  status: {
+    type: String,
+    enum: ["todo", "inProgress", "review", "completed"],
+    default: "todo",
+  },
+  createdBy: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+  assignedTo: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+  assignedToName: { type: String, required: true },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
